6f7f8d278158c830f805860b9645acdf8809045a
Марат Ахметов <ahmetov.marat2015@yandex.ru>
2022-11-03 19:33:44 +0300

    Создал модель User

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    USER = 'US'
+    ADMIN = 'AD'
+    MODERATOR = 'MO'
+    ROLES_CHOICES = [
+        (USER, 'user'),
+        (ADMIN, 'admin'),
+        (MODERATOR, 'moderator'),
+    ]
+
+    email = models.EmailField(
+        unique=True,
+        verbose_name='Адрес электронной почты'
+    )
+    bio = models.TextField(blank=True, verbose_name='Биография')
+    role = models.CharField(
+        max_length=2,
+        choices=ROLES_CHOICES,
+        default=USER,
+        verbose_name='Роль'
+    )
