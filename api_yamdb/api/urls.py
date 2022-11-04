84fb736bc6acf9f6ecf81bd1340481362a62af06
Марат Ахметов <ahmetov.marat2015@yandex.ru>
2022-11-04 12:59:24 +0300

    Подключил JWT токен

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,5 +1,9 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from api.views import CommentViewSet, ReviewViewSet
 
@@ -16,4 +20,7 @@ router_v1.register(r'reviews/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/commen
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
+    path('v1/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('v1/token/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('v1/auth/', include('django.contrib.auth.urls')), 
 ]
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,6 +25,7 @@ INSTALLED_APPS = [
     'api.apps.ApiConfig',
     'reviews.apps.ReviewsConfig',
     'users.apps.UsersConfig',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -107,3 +109,18 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 
 
 AUTH_USER_MODEL = 'users.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
