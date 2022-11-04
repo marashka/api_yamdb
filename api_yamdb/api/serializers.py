87c1a656e74fad2d760afa7073ad9fffb8befa23
Андрей Киселев <artes915@yandex.ru>
2022-11-04 16:07:50 +0300

    ser, urls, views for CGT + stuff

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,36 @@
 from rest_framework import serializers
 
-from reviews.models import Review, Comment
+from reviews.models import Category, Genre, Title, Review, Comment
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = ('name', 'slug')
+
+
+class GenreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Genre
+        fields = ('name', 'slug')
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(
+        slug_field='slug', queryset=Category.objects.all()
+    )
+    genre = serializers.SlugRelatedField(
+        slug_field='slug', many=True, queryset=Genre.objects.all()
+    )
+    # rating = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Title
+        fields = (
+            'id', 'name', 'year', 'category', 'genre', 'rating', 'description'
+        )
 
 
 class ReviewSerializer(serializers.ModelSerializer):
