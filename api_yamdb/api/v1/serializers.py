f18e3348717d7bd817c3b1536700139ed7990cca
SK <serg.7x@yandex.ru>
2022-11-09 23:29:54 +0500

    fixing-003

--- api_yamdb/api/v1/serializers.py
+++ api_yamdb/api/v1/serializers.py
@@ -59,15 +59,15 @@ class ReviewSerializer(serializers.ModelSerializer):
         request = self.context.get('request')
         title_id = self.context.get('view').kwargs.get('title_id')
         title = get_object_or_404(Title, pk=title_id)
-        if request.method == 'POST':
-            # Нарушен патерн:
-            # https://medium.com/lemon-code/guard-clauses-3bc0cd96a2d3
-            if Review.objects.filter(
-                    author=request.user,
-                    title=title).exists():
-                raise serializers.ValidationError(
-                    'Нельзя сделать 2 отзыва на одно произведение!'
-                )
+        if request.method != 'POST':
+            return data
+
+        if Review.objects.filter(
+                author=request.user,
+                title=title).exists():
+            raise serializers.ValidationError(
+                'Нельзя сделать 2 отзыва на одно произведение!'
+            )
 
         return data
 
