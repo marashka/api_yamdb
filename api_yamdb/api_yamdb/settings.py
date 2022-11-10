035d0e1ad58ef78b24143f28a99e6a5f839f3d19
Марат Ахметов <ahmetov.marat2015@yandex.ru>
2022-11-10 21:11:26 +0300

    Переделал получения SECRET_KEY

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,10 +1,7 @@
 import os
 from datetime import timedelta
-from dotenv import load_dotenv
 
 from ultis.exceptions import SecretKeyError
-
-load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
